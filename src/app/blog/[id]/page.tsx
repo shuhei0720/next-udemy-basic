9b0d339adedfd36bf0762ac5adf6bc6284642c83
shuhei_0720 <shuhei_0720@icloud.com>
2025-12-26 23:44:51 +0900

    ページコンポーネントを追加し、不要なスタイルを削除

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,15 @@
+type Params = {
+  params: Promise<{
+    id: string
+  }>
+}
+
+export default async function BlogPage({params} : Params) {
+  // console.log(params)
+  const { id } = await params;
+  return (
+    <div>
+      ブログID：{ id }
+    </div>
+  )
+}
