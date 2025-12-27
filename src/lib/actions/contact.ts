5226fe49386d3842bc405c8105690f6cd6206433
shuhei_0720 <shuhei_0720@icloud.com>
2025-12-27 23:07:18 +0900

    お問い合わせフォームのバリデーション機能を追加し、Zodを使用して入力チェックを実装

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -1,11 +1,18 @@
 'use server'
 import { redirect } from "next/navigation"
+import { ContactSchema } from "@/validations/contact"
 
 export async function submitContactForm(formData: FormData) {
   const name = formData.get('name')
   const email = formData.get('email')
 
   // バリデーション
+  const validationResult = ContactSchema.safeParse({ name, email })
+  if(!validationResult.success){
+    const errors = validationResult.error.flatten()
+    console.log('サーバー側でエラー', errors)
+    return {}
+  }
   // DB登録
 
   console.log('送信されたデータ:', { name, email })
