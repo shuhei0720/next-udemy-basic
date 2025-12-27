04a65371d3b6bc05b585fc44367e62f65d6cd192
shuhei_0720 <shuhei_0720@icloud.com>
2025-12-27 13:54:13 +0900

    クライアントおよびサーバーコンポーネントにリンクを追加

--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -1,4 +1,5 @@
 import ClientComponent from "@/components/ClientComponent"
+import Link from "next/link"
 
 export default function ServerComponent() {
   console.log('Server')
@@ -6,6 +7,7 @@ export default function ServerComponent() {
     <div>
       サーバー
       <ClientComponent />
+      <Link href="/about">About</Link>
     </div>
   )
 }
