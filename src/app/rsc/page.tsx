09dfb9c3b04be5fe47a4a29ab4966af8e869f648
shuhei_0720 <shuhei_0720@icloud.com>
2025-12-27 13:37:40 +0900

    クライアントコンポーネントを追加し、カウント機能を実装

--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -1,8 +1,11 @@
+import ClientComponent from "@/components/ClientComponent"
+
 export default function ServerComponent() {
   console.log('Server')
   return (
     <div>
       サーバー
+      <ClientComponent />
     </div>
   )
 }
