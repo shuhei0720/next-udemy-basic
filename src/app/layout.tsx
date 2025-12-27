bd6f481f123770eeb75d1dd1fea571134d609856
shuhei_0720 <shuhei_0720@icloud.com>
2025-12-27 11:54:30 +0900

    言語を日本語に変更し、Rootヘッダーを追加してページコンポーネントを作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body>
+        <header className="bg-blue-200 p-4">Rootヘッダー</header>
         {children}
       </body>
     </html>
