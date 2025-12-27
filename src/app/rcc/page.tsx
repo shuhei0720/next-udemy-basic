b10a80b3b76501bcb4ae44c1cb9754eb7afcacb7
shuhei_0720 <shuhei_0720@icloud.com>
2025-12-27 13:22:00 +0900

    クライアントおよびサーバーコンポーネントを追加し、それぞれの基本的な表示機能を実装

---
+++ src/app/rcc/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+export default function ClientComponent() {
+  console.log('Client')
+  return (
+    <div>クライアント</div>
+  )
+}
---
+++ src/app/rsc/page.tsx
@@ -0,0 +1,6 @@
+export default function ServerComponent() {
+  console.log('Server')
+  return (
+    <div>サーバー</div>
+  )
+}
