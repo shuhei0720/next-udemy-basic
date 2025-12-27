40f4df67dac13007238354aa03967690eab0c03e
shuhei_0720 <shuhei_0720@icloud.com>
2025-12-27 12:17:19 +0900

    ブログページコンポーネントを追加し、ダミーデータを表示する機能を実装

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,27 @@
+// ダミーデータ
+const articles = [
+  { id: "1", title: "タイトル1"},
+  { id: "2", title: "タイトル2"},
+  { id: "3", title: "タイトル3"},
+]
+
+// 3秒待機
+async function fetchArticles(){
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  return articles
+}
+
+export default async function BlogPage() {
+  const articles = await fetchArticles()
+  return (
+    <div>
+      <ul>
+        { articles.map((article)=>(
+          <li key={article.id}>
+            title: { article.title }
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
